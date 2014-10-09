63a932f7c1be750f1fb6c61cb70c43d273108430
chowyuncat <chowyuncat@gmail.com>
2014-10-09 13:36:10 -0700

    bash: Added current git branch to prompt.

--- bash-prompt.sh
+++ bash-prompt.sh
@@ -1,2 +1,9 @@
-export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\n\$ "
+function setup_prompt {
+	local default_color='\[\e[m\]'
+	local purple="\[\033[35m\]"
+	local __git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`'
+	export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\] $purple$__git_branch$default_color \n\$ "
+}
+
 export HISTCONTROL=erasedups:ignorespace
+setup_prompt
