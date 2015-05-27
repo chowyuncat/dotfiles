d81d714794754585a532828156e78533a29577f3
chowyuncat <no@way.com>
2015-05-27 11:50:08 -0700

    Bash prompt: prepend a timestamp.

--- bash-prompt.sh
+++ bash-prompt.sh
@@ -3,6 +3,9 @@ function setup_prompt {
 	local purple="\[\033[35m\]"
 	local __git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`'
 	export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\] $purple$__git_branch$default_color \n\$ "
+
+	# Prepend a timestamp
+	export PS1="\n  \D{%F %T}\n$PS1"
 }
 
 export HISTCONTROL=erasedups:ignorespace
