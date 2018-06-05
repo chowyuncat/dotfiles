429d83bd57cdffda9fef9694659828a57711ca43
Cat Zimmermann <cat.zimmermann@crowdstrike.com>
2018-06-05 09:34:56 -0700

    Improve Bash prompt

--- bash-prompt.sh
+++ bash-prompt.sh
@@ -1,15 +1,26 @@
 function setup_prompt {
-	local default_color='\[\e[m\]'
-	local purple="\[\033[35m\]"
-	local __git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`'
-	export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\] $purple$__git_branch$default_color \n\$ "
+    local default_color='\e[39m'
+    local purple="\e[35m"
+    local red="\e[31m"
+    local __git_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+    local __git_rev_local=$(git rev-parse --short HEAD 2> /dev/null)
+    local __git_rev_upstream=$(git rev-parse --short @{upstream} 2> /dev/null)
+    if [ "${__git_rev_local}" != "${__git_rev_upstream}" ]; then
+        local __git_rev="${red}*$__git_rev_local / $__git_rev_upstream"
+    else
+        local __git_rev="${purple}$__git_rev_local"
+    fi
 
-	# Prepend a timestamp
-	export PS1="\n  \D{%F %T}\n$PS1"
+    printf "(${__git_branch}) ${__git_rev} ${default_color}"
 }
 
-setup_prompt
+export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]csmbp:\[\033[33;1m\]\w\[\033[m\] \$(setup_prompt) \nλ "
+# export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\] $purple$__git_branch$default_color \nλ "
+
+# Prepend a timestamp
+export PS1="\n ^-- \D{%F %T}\n\n$PS1"
 
 # These commands should be organized into a separate file
 shopt -s histverify
 export HISTCONTROL=erasedups:ignorespace
+set -o ignoreeof
