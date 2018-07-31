6844e46d578be62875794038a2694e4f74c84825
Cat Zimmermann <cat.zimmermann@crowdstrike.com>
2018-07-31 16:00:25 -0700

    Add hot corner: upper left puts display to sleep

--- osx-preferences.sh
+++ osx-preferences.sh
@@ -15,3 +15,9 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 # Allow full keyboard navigation of all UI controls
 # Note: this value is 2 in Yosemite (and higher?), but 2 in older versions of OS X
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 2
+
+
+# Set top-level corner to put display to sleep
+defaults write com.apple.dock.plist "wvous-tl-corner" -int 10
+# Set top-level corner to require shift held down
+defaults write com.apple.dock.plist "wvous-tl-modifier" -int 131072
