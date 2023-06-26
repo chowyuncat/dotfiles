ac87c7df94191e2f30ad4f73ace916677b9a063a
Cat Zimmermann <csz@bathtowel.local>
2023-06-26 14:55:58 -0700

    Improve zsh prompt in git repository

--- zsh-prompt.sh
+++ zsh-prompt.sh
@@ -10,11 +10,33 @@ precmd() { vcs_info }
 # Format the vcs_info_msg_0_ variable
 # %b - branch
 # %i - current revision
-zstyle ':vcs_info:git:*' formats '(%b) %b %i'
+zstyle ':vcs_info:git:*' formats '(%b) %i'
+
+function git_string() {
+    if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1;
+    then
+	return
+    fi
+
+    local default_color='\e[39m'
+    local purple="\e[35m"
+    local red="\e[31m"
+    local __git_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+    local __git_rev_local=$(git rev-parse --short HEAD 2> /dev/null)
+    local __git_rev_upstream=$(git rev-parse --short @{upstream} 2> /dev/null)
+    if [ "${__git_rev_local}" != "${__git_rev_upstream}" ]; then
+        local __git_rev="${red}*$__git_rev_local / $__git_rev_upstream"
+    else
+        local __git_rev="$__git_rev_local"
+    fi
+
+    printf "(${__git_branch}) ${__git_rev} ${default_color}"
+}
  
 # Set up the prompt (with git branch name)
 setopt PROMPT_SUBST
-PROMPT='%n in ${PWD/#$HOME/~} ${vcs_info_msg_0_} > '
+PROMPT='%n@ ${PWD/#$HOME/~} ${vcs_info_msg_0_}λ '
+PROMPT='%n@ ${PWD/#$HOME/~} $(git_string)'$'\n''λ '
 
 ###
 # Updates editor information when the keymap changes.
